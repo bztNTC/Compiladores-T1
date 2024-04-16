64b69402dbfdd7d40a5dc79f322a1fde1a928a7a
bztNTC <brunozcm@estudante.ufscar.br>
2024-04-16 17:29:16 -0300

    Adicionando README.md e comentarios no Principal.java

--- compt1/src/main/java/br/ufscar/dc/compiladores/compt1/Principal.java
+++ compt1/src/main/java/br/ufscar/dc/compiladores/compt1/Principal.java
@@ -11,15 +11,18 @@ public class Principal {
 
     public static void main(String[] args) {
         try {
+            // Verifica se foram passados os argumentos corretamente
             String arquivoSaida = args[1];
             CharStream cs = CharStreams.fromFileName(args[0]);
             try (PrintWriter pw = new PrintWriter(arquivoSaida)) {
                 AlgumaLexer lex = new AlgumaLexer(cs); 
                 Token t = null; 
                 
+                // Loop para ler e processar os tokens
                 while ((t = lex.nextToken()).getType() != Token.EOF) {
                     String nomeToken = AlgumaLexer.VOCABULARY.getDisplayName(t.getType());
 
+                    // Verifica o tipo do token e imprime conforme necessário
                     switch (nomeToken) {
                         case "CARACTER_INVALIDO":
                             pw.println("Linha " + t.getLine() + ": " + t.getText() + " - simbolo nao identificado");
@@ -40,16 +43,19 @@ public class Principal {
                             pw.println("<'" + t.getText() + "'," + nomeToken + ">");
                             break;
                     }
+
+                    // Se encontrar um token de erro, interrompe o loop
                     if (nomeToken.equals("CARACTER_INVALIDO") || nomeToken.equals("CADEIA_SEM_FIM") || nomeToken.equals("COMENTARIO_SEM_FIM")) {
                         break;
                     }
                 }
             }catch(FileNotFoundException ex) { 
+                // Tratamento de exceção para arquivo não encontrado
                 System.err.println("O arquivo/diretório não existe:"+args[1]);
             }
         } catch (IOException errado) { 
+            // Tratamento de exceção para erro de I/O
             errado.printStackTrace();
         }
     }
 }
-
